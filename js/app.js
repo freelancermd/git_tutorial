dfc09c9b4d5f72e132e8e9755bc64918035dbab3
Emon Islam <emonislamtube@gmail.com>
2022-03-16 07:03:21 +0600

    Making a increment function with JavaScript

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-alert("this is the starting page");
+const button = document.querySelector(".button");
+const display = document.querySelector(".display");
+let number = 0;
+button.addEventListener("click",function(){
+    number++;
+    display.textContent = number;
+});
